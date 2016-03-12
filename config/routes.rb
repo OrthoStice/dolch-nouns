df8255a6c6a85aa95d1ed12a7d983a367fa5cb98
Shelley Hicklin <shelleyhicklin@hotmail.com>
2016-03-12 20:09:36 +0000

    personalized content

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,15 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
   resources :students
   resources :users
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
   get 'signup'  => 'users#new'
+  get 'login'   => 'sessions#new'
+  post 'login'  => 'sessions#create'
+  delete 'logout'=> 'sessions#destroy'
 
   
   # The priority is based upon order of creation: first created -> highest priority.
