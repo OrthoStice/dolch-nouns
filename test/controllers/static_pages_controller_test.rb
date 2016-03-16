f329446755ea4870296643a1243122759fc432fe
Shelley Hicklin <shelleyhicklin@hotmail.com>
2016-03-16 13:38:09 +0000

    fixed page titles

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -11,18 +11,18 @@ class StaticPagesControllerTest < ActionController::TestCase
   test "should get help" do
     get :help
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | Dolche Nouns Flashcard App"
   end
 
   test "should get about" do
     get :about
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | Dolche Nouns Flashcard App"
   end
   
   test "should get contact" do
     get :contact
     assert_response :success
-    assert_select "title", "Contact | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Contact | Dolche Nouns Flashcard App"
   end
 end
