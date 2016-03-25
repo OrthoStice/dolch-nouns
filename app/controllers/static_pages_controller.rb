b87dce165078f22ae5ea6f76c4b7892ee7829cd7
Shelley Hicklin <shelleyhicklin@hotmail.com>
2016-03-25 03:30:23 +0000

    home partials

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -1,5 +1,9 @@
 class StaticPagesController < ApplicationController
   def home
+    if logged_in?
+      @quizzes= current_user.quizzes.build
+      @user= current_user
+    end
   end
 
   def help
--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
 
   resources :students
   resources :users
+  resources :quizzes,          only: [:create, :destroy]
   get 'help'    => 'static_pages#help'
   get 'about'   => 'static_pages#about'
   get 'contact' => 'static_pages#contact'
